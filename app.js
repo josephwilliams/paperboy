da011d66be3afb5e7e51abd6f80a402a0abcd57a
josephwilliams <josephwilli12@gmail.com>
2017-04-23 01:58:48 -0700

    refactored app.js

--- app.js
+++ app.js
@@ -3,23 +3,26 @@ import Summarizer from 'summarizer';
 
 const newsApiKey = process.env.NEWSAPI_API_KEY;
 
-const Scheduler = {
-  const scheduleJob = () => {
-    // This rule is standard cron syntax for once per minute.
-    // See http://stackoverflow.com/a/5398044/1252653
-    const rule = '* * * * *';
+function scheduleJob(jobTitle, rule) {
+  console.log(`>>> ${jobTitle} begun`);
+  const job = schedule.scheduleJob(rule, () => {
+    console.log('newsApiKey!', newsApiKey);
+  });
+}
 
-    // Kick off the job
-    const job = schedule.scheduleJob(rule, () => {
-      console.log('newsApiKey!', newsApiKey);
-    });
-  };
+// This rule is standard cron syntax for once per minute.
+// See http://stackoverflow.com/a/5398044/1252653
+// # .---------------- minute (0 - 59)
+// # |  .------------- hour (0 - 23)
+// # |  |  .---------- day of month (1 - 31)
+// # |  |  |  .------- month (1 - 12) OR jan,feb,mar,apr ...
+// # |  |  |  |  .---- day of week (0 - 6) (Sunday=0 or 7) OR sun,mon,tue,wed,thu,fri,sat
+// # |  |  |  |  |
+// # *  *  *  *  * user-name  command to be executed
+const rule = '* * * * *';
 
-  const init = () => {
-    scheduleJob();
-  };
-};
+const jobTitle = 'get the news';
 
 (() => {
-  Scheduler.init();
+  scheduleJob.({ jobTitle, rule });
 })();
