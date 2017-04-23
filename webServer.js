5d6ce4daefbae41df031ea7d35e6ece877a78bc1
josephwilliams <josephwilli12@gmail.com>
2017-04-23 16:25:46 -0700

    web server setup

--- webServer.js
+++ webServer.js
@@ -1,12 +1,70 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const mongodb = require("mongodb");
+const ObjectID = mongodb.ObjectID;
+
+const SNIPPETS_COLLECTION = "snippets";
 
 const app = express();
+app.use(bodyParser.json());
 
-app.get('*', function (req, res) {
-  res.send('hello world');
+// Create a database constiable outside of the database connection callback
+// to reuse the connection pool in your app.
+const db;
+
+// Connect to the database before starting the application server.
+mongodb.MongoClient.connect(process.env.MONGODB_URI, function (err, database) {
+  if (err) {
+    console.log(err);
+    process.exit(1);
+  }
+
+  // Save database object from the callback for reuse.
+  db = database;
+  console.log("Database connection ready");
+
+  // Initialize the app.
+  const server = app.listen(process.env.PORT || 8080, function () {
+    const port = server.address().port;
+    console.log("App now running on port", port);
+  });
 });
 
-app.get('/env', function (req, res) {
-  console.log('>>> env', process.env.NEWSAPI_API_KEY);
+// API ROUTES
+
+// Generic error handler used by all endpoints.
+function handleError(res, reason, message, code) {
+  console.log("ERROR: " + reason);
+  res.status(code || 500).json({"error": message});
+}
+
+app.get("/api/snippets", function(req, res) {
+  db.collection(SNIPPETS_COLLECTION).find({}).toArray(function(err, snippets) {
+    if (err) {
+      handleError(res, err.message, "Failed to get snippets.");
+    } else {
+      res.status(200).json(snippets);
+    }
+  });
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//
