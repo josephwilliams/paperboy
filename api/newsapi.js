e222b4e4afb36be7e6a90145993acc0a70e36e3f
josephwilliams <josephwilli12@gmail.com>
2017-04-23 23:13:09 -0700

    good changes

--- api/newsapi.js
+++ api/newsapi.js
@@ -1,10 +1,24 @@
 const fetch = require('node-fetch');
-const dbClient = require('../utils/dbClient');
+const mongodb = require("mongodb");
+const ObjectID = mongodb.ObjectID;
+// const dbClient = require('../utils/dbClient');
 
 const SNIPPETS_COLLECTION = "snippets";
 
-// get database object for later use
-const db = dbClient();
+// Create a database constiable outside of the database connection callback
+// to reuse the connection pool in your app.
+let db;
+
+// Connect to the database before starting the application server.
+mongodb.MongoClient.connect(process.env.MONGODB_URI, (err, database) => {
+  if (err) {
+    console.log(err);
+    process.exit(1);
+  }
+
+  // Save database object from the callback for reuse.
+  db = database;
+});
 
 // instantiate dotenv, opening up 'process.env'
 require('dotenv').config({path: __dirname + '/.env'});
